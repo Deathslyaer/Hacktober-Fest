e6e3df9503c5431875cd0eca071f389fa2c02eb9
Deathslyaer <136613336+Deathslyaer@users.noreply.github.com>
2024-10-05 13:19:01 +0530

    Problem 31
    Problem 31 is done in C language

--- C/Easy/31.c
+++ C/Easy/31.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int main() 
-    char str[] ="Google developer student clubs"; 
+int main() {
+    char str[] =("Google developer student clubs"); 
     printf("%s\n", str);
     return 0;
 }
